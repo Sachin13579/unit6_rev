1ba194cc1d2495a1f6dd64b6996629ca51107d64
Sachin13579 <97518055+Sachin13579@users.noreply.github.com>
2022-06-02 11:24:27 +0530

    razorpay link and api key

---
+++ razorpay/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+// https://rzp.io/l/oRjcjKSot
+// razorpay
+// key_id=rzp_test_Na48m8gAR18fBN
+// key_secret=e00RLxI2Tb6d7EHbALvQPi9q
